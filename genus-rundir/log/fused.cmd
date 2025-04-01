e17b19fe30931560aafe635b22b203825d7f9fd6
YokoiWing <2200012741@stu.pku.edu.cn>
2025-04-01 16:55:25 +0800

    Delete genus-rundir directory

--- genus-rundir/log/fused.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 19.12-s121_1, built Dec  3 2019 15:07:17
-
-# Date: Tue Apr 01 15:33:32 2025
-# Host: hpc001 (x86_64 w/Linux 3.10.0-1160.el7.x86_64) (24cores*96cpus*2physical cpus*Intel(R) Xeon(R) Gold 6342 CPU @ 2.80GHz 36864KB)
-# OS:   CentOS Linux release 7.9.2009 (Core)
-
-source /home/yukunw/dacs-lab3/experiment/dacs2024/lab3/results/26631dadda_bit16fb6545dd58885bf2431c21988dff9e3bddef7e0086f449065a0c1e66cdebbb80/genus-rundir/scripts/fused_syn.tcl
